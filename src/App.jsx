d4258c43bad01d99eafec3c34f782a49f3ec0c6b
Arita Osmani <aritaaosmani@gmail.com>
2022-02-02 14:02:18 +0100

    Added searched cities to recent

--- src/App.jsx
+++ src/App.jsx
@@ -12,15 +12,34 @@ function App() {
   const [isCelcius, setIsCelcius] = useState(true)
   const [searchedCities, setSearchedCities] = useState([])
 
+
   useEffect(() => {
     if (search === '') return
-    fetch(`https://api.weatherapi.com/v1/forecast.json?key=ad30963771034a06809133027222701&q=${search}&days=7&alerts=yes`).then(res => res.json()).then(res => setWeatherData(res))
+    fetch(`https://api.weatherapi.com/v1/forecast.json?key=ad30963771034a06809133027222701&q=${search}&days=7&alerts=yes`).then(res => res.json()).then(res => {
+      setWeatherData(res)
+      addNewSearchedCity(res.location.name, res.location.country, res.current['temp_c'], res.current.condition.icon, res.current.condition.text)
+    })
   }, [search])
 
   useEffect(() => {
     fetch(`http://localhost:3001/searched`).then(res => res.json()).then(city => setSearchedCities(city))
   }, [])
 
+  function addNewSearchedCity(city, country, temp, icon, text) {
+    const match = searchedCities.find(item => item.city === city)
+    if (match) return
+    return fetch(`http://localhost:3001/searched`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ city: city, country: country, temp: temp, icon: icon, text: text })
+    }).then(res => res.json()).then(res => {
+      const copy = JSON.parse(JSON.stringify(searchedCities))
+      copy.push(res);
+      setSearchedCities(copy)
+    })
+  }
   return (
     <div className="App">
       <Routes>
